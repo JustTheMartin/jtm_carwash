12cea6dec8e4ecc0cdd97bb6a296efd25ae165fd
Martin <74150428+JustTheMartin@users.noreply.github.com>
2023-08-04 18:33:08 +0200

    .

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    '@es_extended/imports.lua',
+    '@basicscripts/vse/blips.lua',
+    'config.lua',
+    'main.lua'
+}
+
+escrow_ignore {
+    'config.lua'
+}
